c0af55e0a1dd8af91db702a4a7022dd48643b95f
Andrew Telleria <atelleria08@gmail.com>
2018-02-15 15:50:25 -0800

    post detail view

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Author
 from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(date_pub__lte=timezone.now()).order_by('date_pub')
     return render(request, 'posts/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'posts/post_detail.html', {'post': post})
