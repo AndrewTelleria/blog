cb97fe361f3c01f0d2990a54a8a28ef18b7a5f19
Andrew Telleria <atelleria08@gmail.com>
2018-02-14 16:04:57 -0800

    added posts

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Author, Post
 
-admin.site.register(Author, Post)
+admin.site.register(Post)
+admin.site.register(Author)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -12,8 +12,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date_created = models.DateField(default=timezone.now)
-    date_pub = models.DateField()
-    date_mod = models.DateField()
+    date_pub = models.DateField(blank=True, null=True)
+    date_mod = models.DateField(blank=True, null=True)
     authors = models.ManyToManyField(Author)
 
     def __str__(self):
