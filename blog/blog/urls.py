05ead1977f1b2998baf3afd84793767701e3dd05
Andrew Telleria <atelleria08@gmail.com>
2018-02-14 16:21:16 -0800

    changed url to path

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.urls import path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('posts.urls')),
+    path('admin/', admin.site.urls),
+    path(r'', include('posts.urls')),
 ]
