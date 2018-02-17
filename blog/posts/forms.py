a10388bad9debb60fd34d88f01dbebca562de69b
Andrew Telleria <atelleria08@gmail.com>
2018-02-16 19:07:02 -0800

    no manytomany field

--- blog/posts/forms.py
+++ blog/posts/forms.py
@@ -1,15 +1,9 @@
 from django import forms
 
-from .models import Post, Author
+from .models import Post
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'body',)
-
-class AuthorForm(forms.ModelForm):
-
-    class Meta:
-        model = Author
-        fields = ('name',)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,20 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-class Author(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.name
 
 class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     body = models.TextField()
     date_created = models.DateField(default=timezone.now)
     date_pub = models.DateField(blank=True, null=True)
     date_mod = models.DateField(blank=True, null=True)
-    authors = models.ManyToManyField(Author)
+    
 
     def publish(self):
         self.date_pub = timezone.now()
