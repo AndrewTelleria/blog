9022683783db570d5bade5a40f38c87bd4734177
Andrew Telleria <atelleria08@gmail.com>
2018-02-20 16:11:52 -0800

    the ability to leave a comment on the post_detail page

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/new/', views.post_new, name='post_new'),
     re_path('post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    re_path('post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name="add_comment_to_post"),
     re_path('post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     re_path('post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     re_path('post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,9 +2,22 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from .models import Post
 
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'posts/add_comment_to_post.html', {'form': form})
 
 
 def post_detail(request, pk):
