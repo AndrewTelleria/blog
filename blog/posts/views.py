c228e245c200c245df619ca4a68babd25427c9f7
Andrew Telleria <atelleria08@gmail.com>
2018-02-20 20:06:07 -0800

    comment

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,16 +1,28 @@
+<<<<<<< HEAD
 from django.contrib.auth.decorators import login_required
+=======
+<<<<<<< HEAD
+from django.shortcuts import render, get_object_or_404
+=======
+>>>>>>> parent of 70f5b37... conflicts resolved
 from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
-
-from .forms import PostForm
+>>>>>>> 7fb98766f5dbb77e4003ff5f56b114ec32fa665a
 from .models import Post
+from django.utils import timezone
+from .forms import PostForm
 
+<<<<<<< HEAD
+=======
+def post_list(request):
+    posts = Post.objects.filter(date_pub__lte=timezone.now()).order_by('date_pub')
+    return render(request, 'posts/post_list.html', {'posts': posts})
 
-
+>>>>>>> parent of 70f5b37... conflicts resolved
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'posts/post_detail.html', {'post': post})
 
+<<<<<<< HEAD
 
 @login_required
 def post_draft_list(request):
@@ -23,9 +35,20 @@ def post_draft_list(request):
 @login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
+=======
+def post_new(request):
+>>>>>>> parent of 70f5b37... conflicts resolved
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
+<<<<<<< HEAD
+            post = p_form.save(commit=False)
+
+        else:
+            p_form = PostForm()
+        # a_form = AuthorForm(request.POST)
+    return render(request, 'posts/post_edit.html', {'form': form,})
+=======
             post = form.save(commit=False)
             post.save()
             return redirect('post_detail', pk=post.pk)
@@ -33,6 +56,7 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'posts/post_edit.html', {'form': form})
 
+<<<<<<< HEAD
 
 def post_list(request):
     posts = Post.objects.filter(date_pub__lte=timezone.now()).order_by('date_pub')
@@ -42,6 +66,10 @@ def post_list(request):
 
 @login_required
 def post_new(request):
+=======
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+>>>>>>> parent of 70f5b37... conflicts resolved
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -50,6 +78,7 @@ def post_new(request):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
+<<<<<<< HEAD
         form = PostForm()
     return render(request, 'posts/post_edit.html', {'form': form,})
 
@@ -68,3 +97,8 @@ def post_remove(request, pk):
 
 
 
+=======
+        form = PostForm(instance=post)
+    return render(request, 'posts/post_edit.html', {'form': form})
+>>>>>>> 7fb98766f5dbb77e4003ff5f56b114ec32fa665a
+>>>>>>> parent of 70f5b37... conflicts resolved
