77bd717985a0c4d5125e4567ec59f12e792e278c
Andrew Telleria <atelleria08@gmail.com>
2018-03-19 07:29:08 -0700

    side nav

---
+++ blog/posts/static/js/script.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("side-n").offsetWidth;
+
+function openNav() {
+	document.getElementById("side-n").style.marginLeft = "0";
+}
+
+function closeNav(){
+	document.getElementById("side-n").style.marginLeft = "-279px";
+}
